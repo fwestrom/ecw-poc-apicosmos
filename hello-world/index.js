bb2e17fa0d0a9ece1a62f1f586b203fc82b89a70
Fred Westrom <fwestrom@gmail.com>
2018-04-26 11:22:26 -0500

    Add hello-world function.

--- hello-world/index.js
+++ hello-world/index.js
@@ -1,7 +1,8 @@
 module.exports = function(context, request) {
     context.log('poc-apicosmos|hello-world| request:', request);
-    context.response
-        .status(200)
-        .send('Hello, world!');
-    context.done();
+    var response = {
+        status: 200,
+        body: 'Hello, world!',
+    };
+    context.done(null, response);
 };
